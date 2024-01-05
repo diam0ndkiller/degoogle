498ee35fb834a52e0217828c29f7d954575bafe7
diam0ndkiller <118304462+diam0ndkiller@users.noreply.github.com>
2024-01-05 09:50:12 +0100

    Update uninstall_google_apps.py

--- scripts/uninstall_google_apps.py
+++ scripts/uninstall_google_apps.py
@@ -2,12 +2,39 @@
 import os
 import subprocess
 import sys
+import time
 
 adb_shell_cmd = "adb shell"
 
 if len(sys.argv) > 1:
 	device = sys.argv[1]
 	adb_shell_cmd = f"adb -s {device} shell"
+
+PREINSTALL_DISCLAIMER = '''
+\x1b[93mDISCLAIMER
+This script REMOVES system applications and other tools you may need.
+
+In general, after running, you won't have any Google-ACCOUNT specific software left on your phone. But because the Android operating system is partially built by Google, you will have a lot of their stuff left. If you DON'T want that, please try other operating systems.
+
+Please first check if you have a supported device for some Android custom ROMS, because they have a better experience in general than changing your current operating system. Look for:
+- /e/os
+- LineageOS
+- CalyxOS
+- GrapheneOS
+
+If you want to continue using some Google services, like syncing your photos or contacts, please PAY ATTENTION and READ DESCRIPTIONS when entering the uninstaller section.
+
+DO READ the descriptions of MARKED (!!!) apps when uninstalling.
+
+ONCE UNINSTALLED, MOST OF THE APPS AREN'T VERY EASY TO RE-INSTALL!
+\x1b[0m'''
+
+def check_disclaimer():
+	print(PREINSTALL_DISCLAIMER)
+	print("Waiting 10 seconds to read...")
+	time.sleep(10)
+	confirm = input("\x1b[91mDID YOU READ THE DISCLAIMER AND ACCEPT ANY POSSIBLE BROKEN FUNCTIONS AFTER UNINSTALL? (Yes/No): \x1b[0m")
+	if confirm.lower() != "yes": return
 
 def adb_init():
 	print("\x1b[93mSee if your device is found:\x1b[0m")
